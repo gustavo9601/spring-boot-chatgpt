422f0f27b730a13d7699690dc61a6b9a09f02fc3
Gustavo Marquez Prieto <32426088+gustavo9601@users.noreply.github.com>
2023-03-19 18:22:32 -0500

    init project

---
+++ src/test/java/com/gmarquez/chatgpt/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmarquez.chatgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
